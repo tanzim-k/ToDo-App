7d0792750411c9c70b9c5425b3e80635550c8b09
Tanzim Kazi <tanzimk96@gmail.com>
2021-02-21 05:07:01 -0500

    created put mapping

--- server/src/main/java/com/example/server/controller/TodoController.java
+++ server/src/main/java/com/example/server/controller/TodoController.java
@@ -5,10 +5,7 @@ import com.example.server.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +21,13 @@ public class TodoController {
         List<TodoItem> todoItems = todoService.fetchAllTodoItems();
 
         return ResponseEntity.ok(todoItems);
+    }
+
+    @PutMapping("/todoItems/{id}")
+    public ResponseEntity<?> updateTodoItem (@PathVariable Integer id, @RequestBody TodoItem todoItem) {
+        List<TodoItem> todoItems = todoService.fetchAllTodoItems();
+
+        TodoItem updatedTodoItem = todoService.updateTodoItem(id, todoItem);
+        return ResponseEntity.ok(updatedTodoItem);
     }
 }
