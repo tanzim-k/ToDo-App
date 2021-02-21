9fc204548f5e0392802567943945b1e4cf16f4b1
Tanzim Kazi <tanzimk96@gmail.com>
2021-02-21 04:14:15 -0500

    console log checkmark to see change

--- client/src/components/todoitem.jsx
+++ client/src/components/todoitem.jsx
@@ -1,7 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const TodoItem = (props) => {
     const [todoItem, setTodoItem] = useState(props.data);
+
+    useEffect(() => {
+        console.log("to do item changed", todoItem)
+    }, [todoItem])
 
     function updateIsDone() {
         setTodoItem({ ...todoItem, isDone: !todoItem.isDone })
