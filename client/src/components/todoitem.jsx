8f31b3977d7190eb51d28534a8ca363fa4f621af
Tanzim Kazi <tanzimk96@gmail.com>
2021-02-21 04:08:40 -0500

    added props and onchange to check and uncheck box

--- client/src/components/todoitem.jsx
+++ client/src/components/todoitem.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const TodoItem = () => {
+const TodoItem = (props) => {
+    const [todoItem, setTodoItem] = useState(props.data);
+
+    function updateIsDone() {
+        setTodoItem({ ...todoItem, isDone: !todoItem.isDone })
+    }
+
     return (
         <>
-            <input type="checkbox" checked={todoItem.isDone} onChange={ } />{" "}
+            <input
+                type="checkbox"
+                checked={todoItem.isDone}
+                onChange={updateIsDone}
+            />
             <span>{todoItem.task}</span>
         </>
     )
