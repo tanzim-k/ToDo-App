0dce278e195016472d166273ccb7c90e648000dc
Tanzim Kazi <tanzimk96@gmail.com>
2021-02-21 02:50:22 -0500

    added todo item to display checkbox, the box is unchecked b/c isdone is set to false

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [todoItems, setTodoItems] = useState(null);
+
 
   useEffect(() => {
     console.log("testing")
 
-    fetch('http://localhost:8080/todoItems').then((response) =>
-      response.json()
-    ).then(todoItems => {
-      console.log(todoItems)
-    });
-  })
+    if (!todoItems) {
+      fetch('http://localhost:8080/todoItems').then((response) =>
+        response.json()
+      ).then((data => {
+        console.log("Todo items list ", data)
+        setTodoItems(data);
+      }));
+    }
+  }, [todoItems])
   return (
-    <div>Hello world</div>
+    <div>
+      {todoItems ? todoItems.map(todoItem => {
+        return <input type="checkbox" checked={todoItem.isDone} />
+      }) : 'loading'}</div>
   );
 }
 
