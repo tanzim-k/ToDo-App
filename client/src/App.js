ba1a17891e37d6c77bfe211692f407ada74171ab
Tanzim Kazi <tanzimk96@gmail.com>
2021-02-21 01:55:22 -0500

    created fetch todoItems

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    console.log("testing")
+  })
   return (
     <div>Hello world</div>
   );
