72e0e4427abd04d799099bedd2c2649fca210637
Tanzim Kazi <tanzimk96@gmail.com>
2021-02-22 19:14:11 -0500

    updated DELETE in corsconfig

--- server/src/main/java/com/example/server/cors/CorsConfig.java
+++ server/src/main/java/com/example/server/cors/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             @Override
             public void  addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedMethods("GET", "POST", "PUT", "Delete")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowedOrigins("http://localhost:3000");
 
