298db68a70fb7ea710d9a2a13cfcf81cb461f5d9
Tanzim Kazi <tanzimk96@gmail.com>
2021-02-22 19:07:51 -0500

    created delete method in repo

--- server/src/main/java/com/example/server/repository/TodoRepository.java
+++ server/src/main/java/com/example/server/repository/TodoRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
 public class TodoRepository {
@@ -28,5 +29,11 @@ public class TodoRepository {
         todoItem.setId(idCounter++);
         todoItems.add(todoItem);
         return todoItem;
+    }
+
+    public void delete(Integer id) {
+        todoItems = todoItems.stream()
+                .filter(todoItem -> !todoItem.getId().equals(id))
+                .collect(Collectors.toList());
     }
 }
